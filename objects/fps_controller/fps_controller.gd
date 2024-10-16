d76cfa08ff6de737b917d29ff35d984b08b46dfc
Bryan Miller <bmiller1008@gmail.com>
2024-10-16 19:31:01 -0400

    Added FPS controller, Apparition object, and Created an Interactable component.

---
+++ objects/fps_controller/fps_controller.gd
@@ -0,0 +1,104 @@
+extends CharacterBody3D
+
+# ------------------------------------------------------------------------------
+# Signals
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+# Constants and ENUMs
+# ------------------------------------------------------------------------------
+const CAMERA_PITCH_ANGLE : float = deg_to_rad(70.0)
+
+# ------------------------------------------------------------------------------
+# Export Variables
+# ------------------------------------------------------------------------------
+@export var speed : float = 5.0
+@export var jump_speed : float = 5.0
+
+# ------------------------------------------------------------------------------
+# Variables
+# ------------------------------------------------------------------------------
+var _mouse_sensitivity : Vector2 = Vector2(0.02, 0.02)
+
+var _gravity : float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var _input : Vector2 = Vector2.ZERO
+var _jump : bool = false
+
+var _interactable : Interactable = null
+
+# ------------------------------------------------------------------------------
+# Onready Variables
+# ------------------------------------------------------------------------------
+@onready var _camera: Camera3D = $Camera
+@onready var _camera_ray: RayCast3D = %CameraRay
+
+
+# ------------------------------------------------------------------------------
+# Setters / Getters
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+# Override Methods
+# ------------------------------------------------------------------------------
+func _ready() -> void:
+	pass
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	if Input.mouse_mode != Input.MOUSE_MODE_CAPTURED:
+		return
+	
+	if event is InputEventMouseMotion:
+		_UpdateView(event.relative * _mouse_sensitivity)
+	elif event.is_action("move_forward") or event.is_action("move_backward") or event.is_action("move_left") or event.is_action("move_right"):
+		_input = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+
+func _physics_process(delta: float) -> void:
+	_CheckForInteractable()
+
+	velocity.y += -_gravity * delta
+	var move_dir : Vector3 = transform.basis * Vector3(_input.x, 0.0, _input.y)
+	velocity.x = move_dir.x * speed
+	velocity.z = move_dir.z * speed
+	
+	move_and_slide()
+	if is_on_floor() and _jump:
+		velocity.y = jump_speed
+	_jump = false
+
+# ------------------------------------------------------------------------------
+# Private Methods
+# ------------------------------------------------------------------------------
+func _UpdateView(look_relative : Vector2) -> void:
+	if _camera == null: return
+	rotate_y(-look_relative.x)
+	_camera.rotate_x(-look_relative.y)
+	_camera.rotation.x = clampf(_camera.rotation.x, -CAMERA_PITCH_ANGLE, CAMERA_PITCH_ANGLE)
+
+
+func _CheckForInteractable() -> void:
+	if _camera_ray.is_colliding():
+		var node : Node3D = _camera_ray.get_collider()
+		if node is Interactable and node != _interactable:
+			_interactable = node
+			node.show_message()
+	elif _interactable != null:
+		_interactable.show_message(false)
+		_interactable = null
+
+
+# ------------------------------------------------------------------------------
+# Public Methods
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+# Handler Methods
+# ------------------------------------------------------------------------------
