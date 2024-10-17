59b5f8b8e92b1344173bacf6f3cea270f05ce72f
Bryan Miller <bmiller1008@gmail.com>
2024-10-17 13:59:44 -0400

    Small bug fixes for AVC

--- scripts/autos/avc.gd
+++ scripts/autos/avc.gd
@@ -46,12 +46,15 @@ func _ready() -> void:
 func _UpdateAudioFromSettings() -> void:
 	var volume : float = Settings.get_value(SETTINGS_SECTION_AUDIO, AUDIO_BUS_MASTER, DEFAULT_BUS_VOLUME_MASTER)
 	_SetBusVolume(AUDIO_BUS_MASTER, volume)
+	bus_volume_changed.emit(AUDIO_BUS_MASTER, volume * VOLUME_MULTIPLIER)
 	
-	volume = Settings.get_value(SETTINGS_SECTION_AUDIO, AUDIO_BUS_MASTER, DEFAULT_BUS_VOLUME_MUSIC)
+	volume = Settings.get_value(SETTINGS_SECTION_AUDIO, AUDIO_BUS_MUSIC, DEFAULT_BUS_VOLUME_MUSIC)
 	_SetBusVolume(AUDIO_BUS_MUSIC, volume)
+	bus_volume_changed.emit(AUDIO_BUS_MUSIC, volume * VOLUME_MULTIPLIER)
 	
-	volume = Settings.get_value(SETTINGS_SECTION_AUDIO, AUDIO_BUS_MASTER, DEFAULT_BUS_VOLUME_SFX)
+	volume = Settings.get_value(SETTINGS_SECTION_AUDIO, AUDIO_BUS_SFX, DEFAULT_BUS_VOLUME_SFX)
 	_SetBusVolume(AUDIO_BUS_SFX, volume)
+	bus_volume_changed.emit(AUDIO_BUS_SFX, volume * VOLUME_MULTIPLIER)
 
 
 func _SetBusVolume(bus : StringName, volume : float) -> void:
@@ -71,7 +74,6 @@ func set_bus_volume(bus : StringName, volume : float, large_val : bool = false) 
 		volume = clampf(volume, 0.0, 1.0) * 1000.0
 	
 	Settings.set_value(SETTINGS_SECTION_AUDIO, bus, volume)
-	bus_volume_changed.emit(bus, volume * VOLUME_MULTIPLIER)
 
 func get_bus_volume(bus : StringName, large_val : bool = false) -> float:
 	var volume : float = 0.0
@@ -100,3 +102,4 @@ func _on_settings_value_changed(section : String, key : String, value : Variant)
 		if typeof(value) != TYPE_FLOAT: return
 		if has_bus(key):
 			_SetBusVolume(key, value)
+			bus_volume_changed.emit(key, value * VOLUME_MULTIPLIER)
